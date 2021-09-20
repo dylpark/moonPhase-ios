c22d4ad0cc921f50af2a3a3570dad26cd2e7d417
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-20 17:59:49 +1000

    Create MoonPhaseNames.swift

---
+++ MoonPhase/Model/MoonPhaseNames.swift
@@ -0,0 +1,19 @@
+//
+//  MoonPhase.swift
+//  MoonPhase
+//
+//  Created by Dylan Park on 23/7/21.
+//  Copyright © 2021 Dylan Park. All rights reserved.
+
+import Foundation
+
+enum MoonPhaseNames: String {
+    case NEW_MOON = "New Moon"
+    case WAXING_CRESCENT = "Waxing Crescent"
+    case FIRST_QUARTER = "First Quarter"
+    case WAXING_GIBBOUS = "Waxing Gibbous"
+    case FULL_MOON = "Full Moon"
+    case WANING_GIBBOUS = "Waning Gibbous"
+    case LAST_QUARTER = "Last Quarter"
+    case WANING_CRESCENT = "Waning Crescent"
+}
